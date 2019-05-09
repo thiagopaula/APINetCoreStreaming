45e8cb283022178d08c294cd6af3497077126d0a
thiagosampaiopaula <thiagosampaiopaula@gmail.com>
2019-05-09 00:52:34 -0300

    Subindo fonte

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using DotNetCoreStreaming.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DotNetCoreStreaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ClienteController : ControllerBase
+    {
+        public IVideoStreamService _videoStreamService { get; set; }
+
+        public ClienteController(IVideoStreamService videoStreamService)
+        {
+            _videoStreamService = videoStreamService;
+        }
+
+        public async Task<FileStreamResult> Get(string name)
+        {
+            try
+            {
+                var stream = await _videoStreamService.GetVideoByName(name);
+                return new FileStreamResult(stream, new MediaTypeHeaderValue("video/mp4").MediaType);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿using DotNetCoreStreaming.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCoreStreaming
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IVideoStreamService, VideoStreamService>();
+
+            services.AddCors();
+
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+                builder.AllowCredentials();
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
