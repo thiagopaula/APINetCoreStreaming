fc2fe32987331edb8719d58f0473061162a6012f
thiagosampaiopaula <thiagosampaiopaula@gmail.com>
2019-05-09 01:20:56 -0300

    teste

--- Startup.cs
+++ Startup.cs
@@ -2,16 +2,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DotNetCoreStreaming
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IVideoStreamService, VideoStreamService>();
+
+          
+             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
+
 
             services.AddCors();
 
